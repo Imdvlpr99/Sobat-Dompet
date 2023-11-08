dde71f07ac3e30890e9a464efb2183fb4d69ac81
Imdvlpr99 <imdvlpr.agency@gmail.com>
2023-11-08 16:57:04 +0700

    modify : change login layout and add function on class

--- app/src/main/java/com/imdvlpr/expensetracker/activity/auth/LoginView.kt
+++ app/src/main/java/com/imdvlpr/expensetracker/activity/auth/LoginView.kt
@@ -8,9 +8,11 @@ import android.text.InputType
 import com.imdvlpr.expensetracker.R
 import com.imdvlpr.expensetracker.databinding.ActivityLoginBinding
 import com.imdvlpr.expensetracker.helper.base.BaseActivity
+import com.imdvlpr.expensetracker.helper.ui.CustomDualTab
 import com.imdvlpr.expensetracker.helper.ui.CustomInputView
-import com.imdvlpr.expensetracker.helper.ui.CustomToolbar
-import com.imdvlpr.expensetracker.helper.utils.getStatusBarHeight
+import com.imdvlpr.expensetracker.helper.utils.setSpannable
+import com.imdvlpr.expensetracker.helper.utils.setTheme
+import com.imdvlpr.expensetracker.helper.utils.setVisible
 import com.imdvlpr.expensetracker.model.Login
 
 class LoginView : BaseActivity() {
@@ -27,42 +29,66 @@ class LoginView : BaseActivity() {
         }
     }
 
+    enum class TYPE { USERNAME, PHONE }
     private lateinit var binding: ActivityLoginBinding
     private var login = Login()
+    private var loginType: TYPE = TYPE.USERNAME
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTheme()
+        initTab()
         initView()
     }
 
-    private fun initView() {
-        binding.customToolbar.apply {
-            setPadding(0, getStatusBarHeight(), 0, 0)
-            setTitle(getString(R.string.login_text))
-            setListener(object : CustomToolbar.ToolbarListener {
-                override fun onBackButtonClick() {
-                    onBackPressedDispatcher.onBackPressed()
-                    finish()
+    private fun initTab() {
+        binding.dualTab.apply {
+            when (getSelected()) {
+                CustomDualTab.SELECTED.LEFT -> {
+                    binding.userNameLayout.setVisible(true)
+                    binding.phoneLayout.setVisible(false)
+                }
+                CustomDualTab.SELECTED.RIGHT -> {
+                    binding.phoneLayout.setVisible(true)
+                    binding.userNameLayout.setVisible(false)
+                }
+            }
+            setListener(object : CustomDualTab.DualTabListener {
+                override fun onLeftClicked() {
+                    loginType = TYPE.USERNAME
+                    binding.userNameLayout.setVisible(true)
+                    binding.phoneLayout.setVisible(false)
+                }
+
+                override fun onRightClicked() {
+                    loginType = TYPE.PHONE
+                    binding.phoneLayout.setVisible(true)
+                    binding.userNameLayout.setVisible(false)
                 }
             })
         }
+    }
 
-        binding.inputPhone.apply {
-            setTitle(getString(R.string.phone_title))
-            setHint(getString(R.string.phone_hint))
-            setInputType(InputType.TYPE_CLASS_NUMBER)
+    private fun initView() {
+        val createAccount = String.format(getString(R.string.login_do_not_have_account), getString(R.string.login_create_account))
+        binding.createAccount.text = setSpannable(createAccount, getString(R.string.login_create_account))
+
+        binding.inputUsername.apply {
+            setTitle(getString(R.string.register_user_name))
+            setHint(getString(R.string.register_user_name_hint))
+            setHelper(true, getString(R.string.forgot_username))
+            setInputType(InputType.TYPE_CLASS_TEXT)
+            setInputFilter(CustomInputView.InputFilter.LOWERCASE_WITH_SPECIAL_CHAR)
             setListener(object : CustomInputView.InputViewListener {
                 override fun afterTextChanged(s: Editable?) {
-                    when (s.toString().length < 12 || s.toString().length > 13) {
-                        true -> setError(true, getString(R.string.phone_error))
-                        false -> {
-                            setError(false)
-                            login.phone = s.toString()
-                            validateInput()
-                        }
-                    }
+                    login.userName = s.toString()
+                    validateInput()
+                }
+
+                override fun onHelperClicked() {
+
                 }
             })
         }
@@ -70,30 +96,51 @@ class LoginView : BaseActivity() {
         binding.inputPassword.apply {
             setTitle(getString(R.string.password_title))
             setHint(getString(R.string.password_hint))
+            setHelper(true, getString(R.string.forgot_password))
             setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+            setInputFilter(CustomInputView.InputFilter.PASSWORD)
             setIsPassword(true)
-            setIndicator(right = true)
+            setIndicator(true)
             setListener(object : CustomInputView.InputViewListener {
                 override fun afterTextChanged(s: Editable?) {
-                    when (s.toString().length < 5 || s.toString().length > 10) {
-                        true -> setError(true, getString(R.string.password_error))
-                        false -> {
-                            setError(false)
-                            login.password = s.toString()
-                            validateInput()
-                        }
-                    }
+                    login.password = s.toString()
+                    validateInput()
+                }
+
+                override fun onHelperClicked() {
+
                 }
             })
         }
 
-        binding.loginBtn.setOnClickListener {
-            startActivity(OtpView.intentLogin(this, login, OtpView.Companion.TYPE.LOGIN))
+        binding.inputPhone.apply {
+            setTitle(getString(R.string.phone_title))
+            setHint(getString(R.string.phone_hint))
+            setHelper(true, getString(R.string.forgot_phone))
+            setInputType(InputType.TYPE_CLASS_NUMBER)
+            setInputFilter(CustomInputView.InputFilter.PHONE)
+            setSuffix("+62")
+            setListener(object : CustomInputView.InputViewListener {
+                override fun afterTextChanged(s: Editable?) {
+                    login.phone = s.toString()
+                    validateInput()
+                }
+
+                override fun onHelperClicked() {
+
+                }
+            })
         }
+
+        /*binding.loginBtn.setOnClickListener {
+            startActivity(OtpView.intentLogin(this, login, OtpView.Companion.TYPE.LOGIN))
+        }*/
     }
 
     private fun validateInput() {
-        binding.loginBtn.isEnabled = binding.inputPhone.getText().isNotEmpty() &&
-                binding.inputPassword.getText().isNotEmpty()
+        when (loginType) {
+            TYPE.USERNAME -> binding.loginUsernameBtn.isEnabled = login.userName.isNotEmpty() && login.password.isNotEmpty()
+            TYPE.PHONE -> binding.loginPhoneBtn.isEnabled = login.phone.isNotEmpty()
+        }
     }
 }
